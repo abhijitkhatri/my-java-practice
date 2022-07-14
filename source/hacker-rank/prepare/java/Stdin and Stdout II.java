f4986fae8d89d88ce533106a12ed0504b3f8b3b2
Abhijit <abhijitkhatri@gmail.com>
2022-07-14 19:08:48 +0530

    Stdin and Stdout II

---
+++ source/hacker-rank/prepare/java/Stdin and Stdout II.java
@@ -0,0 +1,28 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red255\green255\blue254;\red0\green0\blue0;
+\red22\green107\blue41;\red144\green1\blue18;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c99608;\cssrgb\c0\c0\c0;
+\cssrgb\c7451\c48627\c21176;\cssrgb\c63922\c8235\c8235;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 import\cf0 \strokec4 \'a0java.util.Scanner;\cb1 \
+\
+\cf2 \cb3 \strokec2 public\cf0 \strokec4 \'a0\cf2 \strokec2 class\cf0 \strokec4 \'a0Solution\'a0\{\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 \'a0\'a0\'a0\'a0\cf2 \strokec2 public\cf0 \strokec4 \'a0\cf2 \strokec2 static\cf0 \strokec4 \'a0\cf2 \strokec2 void\cf0 \strokec4 \'a0main(String[]\'a0args)\'a0\{\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0Scanner\'a0scan\'a0=\'a0\cf2 \strokec2 new\cf0 \strokec4 \'a0Scanner(System.in);\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0i\'a0=\'a0scan.nextInt();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 double\cf0 \strokec4 \'a0d\'a0=\'a0scan.nextDouble();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0scan.nextLine();\'a0\cf5 \strokec5 //When\'a0you\'a0add\'a0an\'a0integer\'a0and\'a0then\'a0hit\'a0enter\'a0(/n)\'a0,\'a0compler\'a0takes\'a0that\'a0'/n'\'a0as\'a0a\'a0string.\'a0So\'a0when\'a0you\'a0take\'a0input\'a0string\'a0after\'a0integer,\'a0double\'a0etc,\'a0you\'a0must\'a0have\'a0to\'a0add\'a0an\'a0extra\'a0scan().\cf0 \cb1 \strokec4 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0String\'a0s\'a0=\'a0scan.nextLine();\cb1 \
+\
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0System.out.println(\cf6 \strokec6 "String:\'a0"\cf0 \strokec4 \'a0+\'a0s);\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0System.out.println(\cf6 \strokec6 "Double:\'a0"\cf0 \strokec4 \'a0+\'a0d);\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0System.out.println(\cf6 \strokec6 "Int:\'a0"\cf0 \strokec4 \'a0+\'a0i);\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\}\cb1 \
+\cb3 \}}
