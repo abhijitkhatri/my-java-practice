d91bb490e55f0e5c26c92eb0a3d715db3f976861
Abhijit <abhijitkhatri@gmail.com>
2022-07-18 18:37:59 +0530

    Loops II

---
+++ source/hacker-rank/prepare/java/Loops II.java
@@ -0,0 +1,37 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red255\green255\blue254;\red0\green0\blue0;
+\red22\green107\blue41;\red144\green1\blue18;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c99608;\cssrgb\c0\c0\c0;
+\cssrgb\c7451\c48627\c21176;\cssrgb\c63922\c8235\c8235;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 import\cf0 \strokec4 \'a0java.util.*;\cb1 \
+\cf2 \cb3 \strokec2 import\cf0 \strokec4 \'a0java.io.*;\cb1 \
+\
+\cf2 \cb3 \strokec2 class\cf0 \strokec4 \'a0Solution\{\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\cf2 \strokec2 public\cf0 \strokec4 \'a0\cf2 \strokec2 static\cf0 \strokec4 \'a0\cf2 \strokec2 void\cf0 \strokec4 \'a0main(String\'a0[]argh)\{\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0Scanner\'a0in\'a0=\'a0\cf2 \strokec2 new\cf0 \strokec4 \'a0Scanner(System.in);\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0t=in.nextInt();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 for\cf0 \strokec4 (\cf2 \strokec2 int\cf0 \strokec4 \'a0i=\cf5 \strokec5 0\cf0 \strokec4 ;i<t;i++)\{\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0a\'a0=\'a0in.nextInt();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0b\'a0=\'a0in.nextInt();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0n\'a0=\'a0in.nextInt();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0multiplier=\cf5 \strokec5 1\cf0 \strokec4 ;\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 int\cf0 \strokec4 \'a0result=a;\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 for\cf0 \strokec4 (\cf2 \strokec2 int\cf0 \strokec4 \'a0x=\cf5 \strokec5 1\cf0 \strokec4 ;x<=n;x++)\{\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0result\'a0=\'a0result+\'a0(multiplier*b);\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0multiplier\'a0=\'a0multiplier*\cf5 \strokec5 2\cf0 \strokec4 ;\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0System.out.print(result\'a0+\'a0\cf6 \strokec6 "\'a0"\cf0 \strokec4 );\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\}\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0System.out.println();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\}\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0in.close();\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\}\cb1 \
+\cb3 \}\cb1 \
+}
